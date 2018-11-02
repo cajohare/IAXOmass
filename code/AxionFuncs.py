7dd9531715dd879ac0e0768531fdec431280a2d5
cajohare <ciaran.aj.ohare@gmail.com>
2018-11-02 19:14:27 +0100

    fixed various things

--- code/AxionFuncs.py
+++ code/AxionFuncs.py
@@ -1,10 +1,15 @@
-import numpy as np
-from numpy import pi, sqrt, exp, zeros, size, shape, sinc, linspace, trapz, loadtxt, interp
-from scipy.integrate import cumtrapz, quad
-
 #================================AxionFuncs.py=================================#
 # Contains:
+# Functions for calculating Solar Axion fluxes for photon & electron coupling
+# Functions for calculating X-ray spectra in a haloscope
+# Functions to smear X-ray spectra by an angular resolution
+# Script to generate binned X-ray data for given flux+experiment
 #==============================================================================#
+
+
+from numpy import pi, sqrt, exp, zeros, size, shape
+from numpy import sinc, linspace, trapz, loadtxt, interp
+from scipy.integrate import cumtrapz, quad
 
 
 #==============================================================================#
@@ -12,15 +17,7 @@ def AxionFlux_Primakoff(gag,E):
     norm = 6.02e10*(gag/1e-10)**2.0
     return norm*((E**2.481)/exp(E/1.205))
 
-def AxionFlux_Compton(gae,E):
-    norm = 13.314e6*(gae/1e-13)**2.0
-    return norm*((E**2.987)/exp(E*0.776))
-
-def AxionFlux_Brem(gae,E):
-    norm = 26.311e8*(gae/1e-13)**2.0
-    return norm*E*exp(-0.77*E)/(1+0.667*E**1.278)
-
-def AxionFlux_AxioRecomb(gae,E):
+def AxionFlux_Axioelectron(gae,E):
     #Solar differential Axion Flux from the axion electron coupling
     #column 1 = Energy [keV]
     #column 2 = Axion Flux 1/[10^19 keV cm^2 day]
@@ -33,21 +30,31 @@ def AxionFlux_AxioRecomb(gae,E):
     norm = 1e19*(gae/(0.511e-10))**2.0/(3600*24)
     Flux = interp(E,E1,F1)*norm
     return Flux
+
+def AxionFlux_Compton(gae,E):
+    norm = 13.314e6*(gae/1e-13)**2.0
+    return norm*((E**2.987)/exp(E*0.776))
+
+def AxionFlux_Brem(gae,E):
+    norm = 26.311e8*(gae/1e-13)**2.0
+    return norm*E*exp(-0.77*E)/(1+0.667*E**1.278)
+
 #==============================================================================#
 
 
 
-
-
 #==============================================================================#
-def PhotonNumber_Primakoff(E,m_a):
-    norm = 220893
+def PhotonNumber_Primakoff(Flux_scale,E,m_a):
+    norm = Flux_scale*220893
     return norm*((E**2.481)/exp(E/1.205))*(sinc(25380.710659898483/pi*m_a**2.0/E))**2.0
 
 def PhotonNumber_Electron(Flux,E,m_a):
     norm = 220893/(6.02e10)
     return norm*Flux*(sinc(25380.710659898483/pi*m_a**2.0/E))**2.0
+#==============================================================================#
 
+
+#==============================================================================#
 def smear(dN,E,E_res):
     n = size(dN)
     Norm = 1.0/sqrt(2*pi*E_res**2.0)
@@ -70,68 +77,47 @@ def smearFast(dN,E,E_res):
         K = Norm*exp(-(Eint-E[i])**2.0/(2*E_res**2.0))
         dN_smeared[i] = trapz(K*dN[i1:i2],Eint)
     return dN_smeared
+#==============================================================================#
 
 
-def BinnedPhotonNumberTable_Primakoff(m_vals,E_min,E_max,nE_bins,nfine=100,res_on=False): 
+
+
+#==============================================================================#
+def EnergyBins(E_min,E_max,nfine,nE_bins):
     E_bin_edges = linspace(E_min,E_max,nE_bins+1)
     E_bw = (E_max-E_min)/(nE_bins+1.0)
     E_bins = (E_bin_edges[1:]+E_bin_edges[:-1])/2
-
-    # TRAPZ
-    nm = size(m_vals)
-    R1_tab = zeros(shape=(nE_bins,nm))
-    #Ei = linspace(E_min,E_max,nfine*nE_bins)
+    
     Ei = zeros(shape=(nE_bins*nfine))
     for i in range(0,nE_bins):
         Ei[i*nfine:(i+1)*nfine] = linspace(E_bin_edges[i],E_bin_edges[i+1]-E_bw/nfine,nfine)
         
+    return Ei,E_bins
+
+def BinnedPhotonNumberTable(m_vals,E_min,E_max,nE_bins,coupling='Photon',nfine=100,res_on=False): 
+    nm = size(m_vals)
+    R1_tab = zeros(shape=(nE_bins,nm))
+    if coupling=='Electron':
+        Flux = AxionFlux_Axioelectron(1e-10,Ei)
+        dN_func = PhotonNumber_Electron
+    else:
+        Flux = 1.0
+        dN_func = PhotonNumber_Primakoff
+        
+    # Tabulate m != 0 rates
+    Ei,E_bins = EnergyBins(E_min,E_max,nfine,nE_bins)
     for j in range(0,nm):
-        dN = PhotonNumber_Primakoff(Ei,m_vals[j])
+        dN = dN_func(Flux,Ei,m_vals[j])  
         if res_on:
             dN = smear(dN,Ei,E_min)
         for i in range(0,nE_bins):
             Ebin = Ei[i*nfine:(i+1)*nfine]
             dNbin = dN[i*nfine:(i+1)*nfine]
             R1_tab[i,j] = sum(0.5*(Ebin[1:]-Ebin[0:-1])*(dNbin[1:]+dNbin[0:-1]))
-    
-    # Get m = 0 rate
-    R0 = zeros(shape=(nE_bins))
-    dN = PhotonNumber_Primakoff(Ei,0.0)
-    if res_on:
-        dN = smear(dN,Ei,E_min)
-    for i in range(0,nE_bins):
-        Ebin = Ei[i*nfine:(i+1)*nfine]
-        dNbin = dN[i*nfine:(i+1)*nfine]
-        R0[i] = sum(0.5*(Ebin[1:]-Ebin[0:-1])*(dNbin[1:]+dNbin[0:-1]))
-
-    return E_bins,R1_tab,R0
-
-def BinnedPhotonNumberTable_Electron(m_vals,E_min,E_max,nE_bins,nfine=100,res_on=False): 
-    E_bin_edges = linspace(E_min,E_max,nE_bins+1)
-    E_bw = (E_max-E_min)/(nE_bins+1.0)
-    E_bins = (E_bin_edges[1:]+E_bin_edges[:-1])/2
-
-    # TRAPZ
-    nm = size(m_vals)
-    R1_tab = zeros(shape=(nE_bins,nm))
-    #Ei = linspace(E_min,E_max,nfine*nE_bins)
-    Ei = zeros(shape=(nE_bins*nfine))
-    for i in range(0,nE_bins):
-        Ei[i*nfine:(i+1)*nfine] = linspace(E_bin_edges[i],E_bin_edges[i+1]-E_bw/nfine,nfine)
-       
-    Flux = AxionFlux_AxioRecomb(1e-10,Ei)
-    for j in range(0,nm):
-        dN = PhotonNumber_Electron(Flux,Ei,m_vals[j])
-        if res_on:
-            dN = smearFast(dN,Ei,E_min)
-        for i in range(0,nE_bins):
-            Ebin = Ei[i*nfine:(i+1)*nfine]
-            dNbin = dN[i*nfine:(i+1)*nfine]
-            R1_tab[i,j] = sum(0.5*(Ebin[1:]-Ebin[0:-1])*(dNbin[1:]+dNbin[0:-1]))
         
     # Get m = 0 rate
     R0 = zeros(shape=(nE_bins))
-    dN =  PhotonNumber_Electron(Flux,Ei,0.0)
+    dN =  dN_func(Flux,Ei,0.0)
     if res_on:
         dN = smear(dN,Ei,E_min)
     for i in range(0,nE_bins):
@@ -140,4 +126,4 @@ def BinnedPhotonNumberTable_Electron(m_vals,E_min,E_max,nE_bins,nfine=100,res_on
         R0[i] = sum(0.5*(Ebin[1:]-Ebin[0:-1])*(dNbin[1:]+dNbin[0:-1]))
 
     return E_bins,R1_tab,R0
-
+#==============================================================================#
--- code/Like.py
+++ code/Like.py
@@ -1,17 +1,21 @@
+#================================Like.py=======================================#
+# Contains:
+# 2-D and 1-D profile likelihood functions for axion coupling and mass
+# Function that calculates mass discovery limits at 3 sigma using Asimov data
+# Function that calculates constant event number lines
+#
+#==============================================================================#
+
+
 from numpy import pi, sqrt, exp, zeros, size, shape, sinc, linspace, logspace
 from numpy import log10, floor, log, interp, append, flipud
 from scipy.integrate import cumtrapz, quad
 from iminuit import minimize
 from scipy.special import gammaln
 
-#================================Like.py=======================================#
-# Contains:
 #==============================================================================#
-
-
-def lnPF(Nob,Nex):
-    return sum(Nob*log(Nex) - Nex)# - gammaln(Nob+1.0)) #factorial removed for speed
-
+# Important function that interpolates N_exp events at an arbitrary mass (m)
+# using the tabulated data stored in R1_tab, then rescales by g
 def InterpExpectedEvents(g,m,m_vals,R1_tab):
     nm = size(m_vals)
     m1 = m_vals[0]
@@ -20,7 +24,17 @@ def InterpExpectedEvents(g,m,m_vals,R1_tab):
     i2 = i1+1
     N_exp = 1e40*(g**4.0)*((R1_tab[:,i1]*(m_vals[i2]-m)+R1_tab[:,i2]*(m - m_vals[i1]))/(m_vals[i2]- m_vals[i1]))
     return N_exp
+#==============================================================================#
 
+
+#==============================================================================#
+# Likelihoods:
+
+# Log of poisson pdf
+def lnPF(Nob,Nex):
+    return sum(Nob*log(Nex) - Nex)# - gammaln(Nob+1.0)) #factorial removed for speed
+
+# 2D likelihood for (g,m)
 def llhood2(X,N_obs,m_vals,R1_tab):
     m = X[1]
     g = 10.0**X[0]
@@ -28,23 +42,44 @@ def llhood2(X,N_obs,m_vals,R1_tab):
     LL = -1.0*lnPF(N_obs,N_exp)
     return LL
     
+# Profile likelihood for (m)
 def llhood2_marg(m,N_obs,m_vals,R1_tab):
     N_exp_10 = InterpExpectedEvents(1e-10,m,m_vals,R1_tab)
     g0 = ((sum(N_obs)/sum(N_exp_10))**0.25)*1e-10
     LL = llhood2([log10(g0),m],N_obs,m_vals,R1_tab)
     return LL
 
+# Profile likelihood for (m=0)
 def llhood2_marg0(N_obs,R0):
     N_exp = (sum(N_obs)/sum(R0))*R0
     LL = -1.0*lnPF(N_obs,N_exp)
     return LL
-    
+
+# Profile likelihood for (g,m=0)
 def llhood1(X,N_obs,R0):
     g = 10.0**X[0]
     N_exp = 1e40*(g**4.0)*R0
     LL = -1.0*lnPF(N_obs,N_exp)
     return LL
+#==============================================================================#
 
+
+
+
+#==============================================================================#
+def ConstantObsNumberLine(N_ob,mi,m_vals,R1_tab):
+    ni = size(mi)
+    g = zeros(shape=ni)
+    for i in range(0,ni):
+        N_exp_10 = InterpExpectedEvents(1e-10,mi[i],m_vals,R1_tab)
+        g[i] = 1e-10*(N_ob/sum(N_exp_10))**0.25
+    return g
+#==============================================================================#
+
+
+#==============================================================================#
+# Simple analytic calculation of mass discovery limit using formulae derived in
+# the paper.
 def MassDiscoveryLimit_Simple(m_vals,R1_tab,R0,m_DL_vals):
     nm = size(m_vals)
     n_DL = size(m_DL_vals)
@@ -58,7 +93,9 @@ def MassDiscoveryLimit_Simple(m_vals,R1_tab,R0,m_DL_vals):
         DL[im] = 1e-10*(9.0/sum(2*N*log(N/(D*N0))))**0.25
     return DL
 
-def MassDiscoveryLimit(m_vals,R1_tab,R0,m_DL_vals,gmin=1e-12,gmax=1e-7,ng=100):
+# Algorithmic calculation of the discovery limit using Minuit likelihood 
+# minimisation (currently unused but will be needed for future studies)
+def MassDiscoveryLimit_Minuit(m_vals,R1_tab,R0,m_DL_vals,gmin=1e-12,gmax=1e-7,ng=100):
     nm = size(m_vals)
     n_DL = size(m_DL_vals)
     DL = zeros(shape=(n_DL))
@@ -78,9 +115,8 @@ def MassDiscoveryLimit(m_vals,R1_tab,R0,m_DL_vals,gmin=1e-12,gmax=1e-7,ng=100):
 
                 #------ Massless case ------#
                 X_in1 = [log10(g)]
-                L1 = llhood2_marg0(N_obs,R0)
-                #res = minimize(llhood1, X_in1, args=(N_obs,R0))
-                #L1 = res.fun
+                res = minimize(llhood1, X_in1, args=(N_obs,R0))
+                L1 = res.fun
 
                 # Test statistic
                 D12 = -2.0*(L2-L1) # significance for measuring mass
@@ -90,30 +126,82 @@ def MassDiscoveryLimit(m_vals,R1_tab,R0,m_DL_vals,gmin=1e-12,gmax=1e-7,ng=100):
                 g_prev = g # Reset for interpolation
                 D12_prev = D12
     return DL
+#==============================================================================#
 
 
-def CouplingDiscoveryLimit(m_vals,R1_tab,m_DL_vals,gmin=1e-12,gmax=1e-7,ng=100):
+#==============================================================================#
+# Function for calculating the mass estimation discovery limit 
+# (Fig.6 of the paper)
+# Can work in two modes:
+#
+# Mode 1 when gmin_vals = scalar: The scan over coupling values begins with 
+# the value that produced N_obs=2 events
+#
+# Mode 2 (when gmin_vals = array of size (n_DL)): The scan begins at the values 
+# specified
+#
+# The two modes are needed because the first scan over the parameter space is needed
+# to eliminate all of the spurious regions at low and high masses that give rise 
+# to likelihoods within the confidence interval band 
+# (i.e. likelihood ratio>-4 for example) but are no where near the correct mass 
+# (i.e. within err away from m0)
+def MassEstimationDiscoveryLimit(err,m_vals,R0,R1_tab,m_DL_vals,sigmas=2,gmin_vals=1.0,gmax=1e-9,ng=500,nL=1000):
+    itot = nL
     nm = size(m_vals)
     n_DL = size(m_DL_vals)
-    DL0 = zeros(shape=(n_DL))
-    g_vals = logspace(log10(gmin),log10(gmax),ng)
+    DLmerr = zeros(shape=(n_DL))
     for im in range(0,n_DL):
+        m0 = m_DL_vals[im]
+        i0 = int(floor((log10(m0)-log10(m_vals[0]))\
+                       *(nm-1.0)/(log10(m_vals[-1])-log10(m_vals[0]))+1.0))-1
+        iupper = int(itot*i0/nm)
+        ilower = itot-iupper
+        mlower = flipud(logspace(log10(m_vals[1]),log10(m0*(1-err/2)),ilower))
+        mupper = logspace(log10(m0*(1+err/2)),log10(m_vals[-2]),iupper)
+        if size(gmin_vals)>1:
+            gmin = gmin_vals[im]
+        else:
+            N = R1_tab[:,i0]
+            N0 = R0
+            D = sum(R1_tab[:,i0])/sum(R0)
+            gmin = 1e-10*(1.0/sum(2*N*log(N/(D*N0))))**0.25
+            
+        g_vals = logspace(log10(gmin),log10(gmax),ng)
         for j in range(0,ng):
-            g = g_vals[j]
-            m0 = m_DL_vals[im]
-            N_obs = InterpExpectedEvents(g,m0,m_vals,R1_tab)
-            if sum(N_obs)>4.9:
-                DL0[im] = g
-                break
-    return DL0
+            g0 = g_vals[j]
+            N_obs = R1_tab[:,i0]*(g0/1e-10)**4.0
+            Lmax = llhood2_marg(m0,N_obs,m_vals,R1_tab)
+            Lprof0 = -2*(llhood2_marg0(N_obs,R0) - Lmax) 
+            Lprofend = -2*(llhood2_marg(m_vals[-2],N_obs,m_vals,R1_tab) - Lmax)
+            if (Lprof0<-sigmas**2.0)&(Lprofend<-sigmas**2.0):
+                c = True
+                for ii in range(0,ilower):
+                    Lprof = -2*(llhood2_marg(mlower[ii],N_obs,m_vals,R1_tab) - Lmax)
+                    if Lprof>-sigmas**2.0:
+                        c = False
+                        break
+                for ii in range(0,iupper):
+                    Lprof = -2*(llhood2_marg(mupper[ii],N_obs,m_vals,R1_tab) - Lmax)
+                    if Lprof>-sigmas**2.0:
+                        c = False
+                        break    
+                if c:
+                    DLmerr[im] = g0
+                    break
+                    
+    return DLmerr
+#==============================================================================#
 
 
-def MassMeasurement(m0,g0,err_l,err_u,sig,m_vals,R1_tab,nL=100): 
+
+
+#==============================================================================#
+# Given an input mass and coupling (m0,g0), calculates the confidence interval
+# around the best fit mass to a given significance level (sigmas)
+# also requires err_l and err_u which is roughly the fractional size over which
+# to search (needs to be very large ~100% for most values of m0)
+def MassMeasurement(m0,g0,err_l,err_u,sigmas,m_vals,R1_tab,nL=100): 
     nm = size(m_vals)
-    if sig==1:
-        crit = -1
-    elif sig==2:
-        crit = -4
     mlim = []
     N_obs = InterpExpectedEvents(g0,m0,m_vals,R1_tab)
     mi = linspace((1-err_l)*m0,(1+err_u)*m0,nL)
@@ -124,16 +212,34 @@ def MassMeasurement(m0,g0,err_l,err_u,sig,m_vals,R1_tab,nL=100):
         m = mi[ii]
         dL = -2*(llhood2_marg(m,N_obs,m_vals,R1_tab)-maxL)
         if up:
-            if dL>crit:
+            if dL>-sigmas**2.0:
                 mlim = append(mlim,m)
                 up = False
         else:
-            if dL<crit:
-                 mlim = append(mlim,m)
-                 up = True
+            if dL<-sigmas**2.0:
+                mlim = append(mlim,m)
+                up = True
     return mlim
+#==============================================================================#
 
 
+
+
+
+
+
+#==============================================================================#
+#==============================================================================#
+#==============================================================================#
+#==============================================================================#
+#==============================================================================#
+
+
+# OLD CODE:
+
+#==============================================================================#
+# Old way of calculating the Mass estimate discovery limit, currently it fails 
+# for most axion masses because the likelihood is so badly behaved
 def MassErrorDiscoveryLimit_Old(err,m_vals,R0,R1_tab,m_DL_vals,gmin=1e-12,gmax=1e-7,ng=100,nL=100):
     nLike = nL
     nm = size(m_vals)
@@ -162,52 +268,4 @@ def MassErrorDiscoveryLimit_Old(err,m_vals,R0,R1_tab,m_DL_vals,gmin=1e-12,gmax=1
                             break
                               
     return DLmerr
-
-def MassErrorDiscoveryLimit(err,m_vals,R0,R1_tab,m_DL_vals,gmax=1e-9,ng=1000,nL=1000):
-    itot = nL
-    nm = size(m_vals)
-    n_DL = size(m_DL_vals)
-    DLmerr = zeros(shape=(n_DL))
-    for im in range(0,n_DL):
-        m0 = m_DL_vals[im]
-        i0 = int(floor((log10(m0)-log10(m_vals[0]))*(nm-1.0)/(log10(m_vals[-1])-log10(m_vals[0]))+1.0))-1
-        iupper = int(itot*i0/nm)
-        ilower = itot-iupper
-        mlower = flipud(logspace(log10(m_vals[1]),log10(m0*(1-err/2)),ilower))
-        mupper = logspace(log10(m0*(1+err/2)),log10(m_vals[-2]),iupper)
-        N = R1_tab[:,i0]
-        N0 = R0
-        D = sum(R1_tab[:,i0])/sum(R0)
-        gmin = 1e-10*(1.0/sum(2*N*log(N/(D*N0))))**0.25
-        g_vals = logspace(log10(gmin),log10(gmax),ng)
-        for j in range(0,ng):
-            g0 = g_vals[j]
-            N_obs = R1_tab[:,i0]*(g0/1e-10)**4.0
-            Lmax = llhood2_marg(m0,N_obs,m_vals,R1_tab)
-            Lprof0 = -2*(llhood2_marg0(N_obs,R0) - Lmax) 
-            Lprofend = -2*(llhood2_marg(m_vals[-2],N_obs,m_vals,R1_tab) - Lmax)
-            if (Lprof0<-1)&(Lprofend<-1):
-                c = True
-                for ii in range(0,ilower):
-                    Lprof = -2*(llhood2_marg(mlower[ii],N_obs,m_vals,R1_tab) - Lmax)
-                    if Lprof>-1:
-                        c = False
-                        break
-                for ii in range(0,iupper):
-                    Lprof = -2*(llhood2_marg(mupper[ii],N_obs,m_vals,R1_tab) - Lmax)
-                    if Lprof>-1:
-                        c = False
-                        break    
-                if c:
-                    DLmerr[im] = g0
-                    break
-                    
-    return DLmerr
-                
-def ConstantObsNumberLine(N_ob,mi,m_vals,R1_tab):
-    ni = size(mi)
-    g = zeros(shape=ni)
-    for i in range(0,ni):
-        N_exp_10 = InterpExpectedEvents(1e-10,mi[i],m_vals,R1_tab)
-        g[i] = 1e-10*(N_ob/sum(N_exp_10))**0.25
-    return g
+#==============================================================================#
